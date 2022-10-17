a80b6a640e94e173b94b1e82db24705e1701d8ff
YUJOO-LEE <lllllllllee@gmail.com>
2022-10-17 11:45:14 +0900

    교통 상황 표시 버튼 추가

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -1,5 +1,5 @@
 import Layout from '../common/Layout';
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 
 export default function Location() {
   const { kakao } = window;
@@ -9,6 +9,11 @@ export default function Location() {
 
   const container = useRef(null);
   // 지도를 담을 영역의 DOM 레퍼런스
+  const [ isTraffic, setTraffic] = useState(false);
+  // 트래픽 표시 여부
+
+  let [ location, setLocation ] = useState(null);
+  // useEffect에서 생성된 인스턴스 담을 State 생성
   const option = { 
   // 지도를 생성할 때 필요한 기본 옵션
     center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
@@ -35,11 +40,26 @@ export default function Location() {
     // 지도 생성 및 객체 리턴
     marker.setMap(mapInstance);
     // 지도상에 마커 표시
+    setLocation(mapInstance);
+    // location State 업데이트
   }, [])
 
+  useEffect(() => {
+    if (!location) return;
+    // location 값 비어있을 시 오류 방지
+
+    isTraffic
+    ?location.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
+    :location.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+    // 교통 상황 표시 활성화
+
+  }, [isTraffic])
+
   return (
-    <Layout name='location'>
+    <Layout name="location">
       <div id="map" ref={container}></div>
+      <button onClick={()=>{setTraffic(!isTraffic)}}>{isTraffic ? 'Traffic Off' : 'Traffic On'}</button>
     </Layout>
   );
+  // state 값에 따라 버튼내 텍스트 변경
 }
