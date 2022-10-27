66eda3f6e8b4e70f3a3930372cbf584ecc91c7b2
YUJOO-LEE <lllllllllee@gmail.com>
2022-10-27 12:23:53 +0900

    메인화면 youtube 리스트 출력

--- src/components/main/Vids.js
+++ src/components/main/Vids.js
@@ -1,3 +1,4 @@
+import { useSelector } from 'react-redux';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper';
 import 'swiper/css';
@@ -5,6 +6,11 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
 export default function Vids() {
+
+  const Vids = useSelector((store)=>{
+  return (store.youtubeReducer.youtube);
+});
+
   return (
     <main id='vids' className='myScroll'>
       <Swiper
@@ -15,34 +21,36 @@ export default function Vids() {
         navigation={true}
         spaceBetween={60}
         loop={true}
-        slidesPerView={1}
+        slidesPerView={3}
         centeredSlides={true}
       >
-        <SwiperSlide>
-          <div className="inner">
-            1
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="inner">
-            2
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="inner">
-            3
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="inner">
-            4
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="inner">
-            5
-          </div>
-        </SwiperSlide>
+        {Vids?.map((data, idx)=>{
+          if (idx > 4) return;
+          // 5개만 출력
+
+          let title = data.snippet.title;
+          let description = data.snippet.description;
+          let date = data.snippet.publishedAt;
+
+          if(title.length > 30) title = title.substring(0, 30) + '...';
+          if(description.length > 100) description = description.substring(0, 100) + '...';
+          date = date.split('T')[0];
+
+          return (
+            <SwiperSlide key={idx}>
+              <div className="inner">
+                <div className="pic">
+                  <img src={data.snippet.thumbnails.standard.url} alt={data.snippet.title} />
+                </div>
+                <div className="txt">
+                  <h3>{title}</h3>
+                  <p>{description}</p>
+                  <span>{date}</span>
+                </div>
+              </div>
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
     </main>
   );
