0dbd05e0046e16034c28c3b713de5aace898a59d
YUJOO-LEE <lllllllllee@gmail.com>
2022-10-27 14:15:00 +0900

    유튜브 데이터 있어야 슬라이드 노출되도록 수정 (버그 방지)

--- src/components/main/Vids.js
+++ src/components/main/Vids.js
@@ -13,45 +13,47 @@ export default function Vids() {
 
   return (
     <main id='vids' className='myScroll'>
-      <Swiper
-        modules={[Pagination]}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        spaceBetween={60}
-        loop={true}
-        slidesPerView={3}
-        centeredSlides={true}
-      >
-        {Vids?.map((data, idx)=>{
-          if (idx > 4) return;
-          // 5개만 출력
+      {Vids.length > 0 &&
+        <Swiper
+          modules={[Pagination]}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          spaceBetween={60}
+          loop={true}
+          slidesPerView={3}
+          centeredSlides={true}
+        >
+          {Vids.map((data, idx)=>{
+            if (idx > 4) return;
+            // 5개만 출력
 
-          let title = data.snippet.title;
-          let description = data.snippet.description;
-          let date = data.snippet.publishedAt;
+            let title = data.snippet.title;
+            let description = data.snippet.description;
+            let date = data.snippet.publishedAt;
 
-          if(title.length > 30) title = title.substring(0, 30) + '...';
-          if(description.length > 100) description = description.substring(0, 100) + '...';
-          date = date.split('T')[0];
+            if(title.length > 30) title = title.substring(0, 30) + '...';
+            if(description.length > 100) description = description.substring(0, 100) + '...';
+            date = date.split('T')[0];
 
-          return (
-            <SwiperSlide key={idx}>
-              <div className="inner">
-                <div className="pic">
-                  <img src={data.snippet.thumbnails.standard.url} alt={data.snippet.title} />
+            return (
+              <SwiperSlide key={idx}>
+                <div className="inner">
+                  <div className="pic">
+                    <img src={data.snippet.thumbnails.standard.url} alt={data.snippet.title} />
+                  </div>
+                  <div className="txt">
+                    <h3>{title}</h3>
+                    <p>{description}</p>
+                    <span>{date}</span>
+                  </div>
                 </div>
-                <div className="txt">
-                  <h3>{title}</h3>
-                  <p>{description}</p>
-                  <span>{date}</span>
-                </div>
-              </div>
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      }
     </main>
   );
 }
