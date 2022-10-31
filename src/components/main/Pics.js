0c2f463a76a72bb90c9cae92cb7c86ee1d14485d
YUJOO-LEE <lllllllllee@gmail.com>
2022-10-31 15:30:10 +0900

    redux-toolkit 설치, 전역으로 관리할 youtube 데이터 slice 파일 생성

--- src/components/main/Pics.js
+++ src/components/main/Pics.js
@@ -10,7 +10,7 @@ export default function Pics(props) {
   return (
     <main id='pics' className='myScroll' ref={pics}>
       <h1 style={{left: position / picsHeight * 200 + '%', opacity: position / picsHeight + 1}}>FLICKR</h1>
-      <h2 style={{top: position / picsHeight * 200 + '%', opacity: position / picsHeight + 1}}>{position}, {picsHeight}</h2>
+      <h2 style={{top: Math.min(position / picsHeight * 200, 100) + '%', opacity: position / picsHeight + 1}}>{position}, {picsHeight}</h2>
       <p style={{left: position}}></p>
     </main>
   );
